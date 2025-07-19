a4d2eb584760bfe108e94bb2383677bb69af0e89
asurya2956 <arysuryapurnama@gmail.com>
2025-07-19 07:19:50 +0700

    Add .next/static/chunks/src_app_dashboard_history_page_tsx_a993527f._.js 1752884387739

---
+++ .next/static/chunks/src_app_dashboard_history_page_tsx_a993527f._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_d5caff2e._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_lodash_20634561._.js",
+  "static/chunks/node_modules_recharts_es6_6b39b0e0._.js",
+  "static/chunks/node_modules_@radix-ui_54a5e052._.js",
+  "static/chunks/node_modules_@floating-ui_9ec1fa39._.js",
+  "static/chunks/node_modules_1d666765._.js"
+],
+    source: "dynamic"
+});
